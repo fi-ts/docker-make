4209fe05f573e38e38add1f240f67bdf191b1b58
Gerrit <tirreg91@googlemail.com>
2020-08-05 17:42:00 +0200

    Do not use inspect.stack().
    It's not very stable and also seems to behave buggy in PyInstaller. Resolves issues like: 
      File "tokenize.py", line 330, in find_cookie
    
    UnicodeDecodeError: 'utf-8' codec can't decode byte 0x95 in position 24: invalid start byte
    
    Can occur during lint phase.

--- dockermake/lint/rules/last_stage.py
+++ dockermake/lint/rules/last_stage.py
@@ -8,7 +8,7 @@ class LastStageRules(RulesBase):
         """LABEL maintainer must occur at most once in last stage"""
         instructions = self.dockerfile.get_maintainer_labels(True, stage=self.dockerfile.get_last_stage())
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_1_0)
 
     def rule0_1_1(self):
         """MAINTAINER must occur at most once in last stage"""
@@ -16,7 +16,7 @@ class LastStageRules(RulesBase):
             Keywords.MAINTAINER, stage=self.dockerfile.get_last_stage()
         )
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_1_1)
 
     def rule0_1_3(self):
         """Either LABEL maintainer or MAINTAINER is mandatory in last stage"""
@@ -24,13 +24,13 @@ class LastStageRules(RulesBase):
         maintainer_labels = self.dockerfile.get_maintainer_labels(True, stage=last_stage)
         maintainer_instructions = self.dockerfile.get_instructions_of_type(Keywords.MAINTAINER, stage=last_stage)
         if not maintainer_labels and not maintainer_instructions:
-            self.error()
+            self.error(self.rule0_1_3)
 
     def rule0_2(self):
         """EXPOSE must occur at most once in last stage"""
         instructions = self.dockerfile.get_instructions_of_type(Keywords.EXPOSE, stage=self.dockerfile.get_last_stage())
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_2)
 
     def rule0_3(self):
         """ENTRYPOINT must occur at most once in last stage"""
@@ -38,19 +38,19 @@ class LastStageRules(RulesBase):
             Keywords.ENTRYPOINT, stage=self.dockerfile.get_last_stage()
         )
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_3)
 
     def rule0_4(self):
         """CMD must occur at most once in last stage"""
         instructions = self.dockerfile.get_instructions_of_type(Keywords.CMD, stage=self.dockerfile.get_last_stage())
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_4)
 
     def rule0_5(self):
         """VOLUME must occur at most once in last stage"""
         instructions = self.dockerfile.get_instructions_of_type(Keywords.VOLUME, stage=self.dockerfile.get_last_stage())
         if len(instructions) > 1:
-            self.error()
+            self.error(self.rule0_5)
 
     def rule7(self):
         """CMD must be after ENTRYPOINT if both are specified in last stage"""
@@ -58,4 +58,4 @@ class LastStageRules(RulesBase):
         cmd_index = self.dockerfile.get_last_index_of(Keywords.CMD, stage=last_stage)
         entrypoint_index = self.dockerfile.get_last_index_of(Keywords.ENTRYPOINT, stage=last_stage)
         if cmd_index != -1 and entrypoint_index != -1 and entrypoint_index > cmd_index:
-            self.error()
+            self.error(self.rule7)
